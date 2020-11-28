2aac077a3be4e4fc645d09a36665113e607a6d33
MunimR <munim.rah123456@gmail.com>
2020-11-27 23:21:40 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <Router>
+
+      <div className="navigation">  
+        <h2>logo</h2>
+        <div className="links">
+          <Link to="/">Home</Link>
+          <Link to="/module">Module</Link>
+          <Link to="/profile">Profile</Link>
+        </div>
+      </div>
+      
+      <div className="content">
+        <Switch>
+          <Route path="/module">
+            <About/>
+          </Route>
+          <Route path="/profile">
+            <Users/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+      </div>
+      
+      </Router>
+
+
+      <div className="footer">
+      </div>
+
     </div>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
